a5ca6ecf7dbdd4b592663fea255ac0fe1390b046
romance <66000596+romancewastaken@users.noreply.github.com>
2024-01-07 14:16:02 -0500

    Add files via upload

--- consoleWndw.cs
+++ consoleWndw.cs
@@ -2,34 +2,62 @@
 using System.Windows.Forms;
 using abuse.handler;
 using System.Threading;
+using libc.hwid;
 
 namespace abuse
 {
     internal class Program
     {
+        public static string hwidType;
         static Thread thread = new Thread((ThreadStart)(() => 
         {
-            try
+            if (hwidType == "1")
             {
-                string hwid = $"{hardwareIdentification.cpuId()} - {hardwareIdentification.diskId()} - {hardwareIdentification.biosId()} - {hardwareIdentification.videoId()}";
+                try
+                {
+                    string hwid = $"{hardwareIdentification.cpuId()} - {hardwareIdentification.diskId()} - {hardwareIdentification.biosId()} - {hardwareIdentification.videoId()}";
+                    Clipboard.SetText(hwid);
+                    Console.Clear();
+                    Console.WriteLine($"HWID: {hwid}");
+                    consoleManager.centerText("Copied minecraft HWID to clipboard");
+                    consoleManager.keepOpen();
+                }
+                catch (Exception ex)
+                {
+                    consoleManager.centerText($"Error grabbing minecraft HWID. Error: {ex.Message}");
+                    consoleManager.keepOpen();
+                    Clipboard.SetText(ex.Message);
+                }
+            }
+            if (hwidType == "2")
+            {
+                var hwid = HwId.Generate();
                 Clipboard.SetText(hwid);
                 Console.Clear();
-                Console.WriteLine($"HWID: {hwid}");
-                consoleManager.centerText("Copied HWID to clipboard");
+                consoleManager.centerText($"HWID: {hwid}");
+                consoleManager.centerText("Copied counter strike HWID to clipboard");
                 consoleManager.keepOpen();
             }
-            catch (Exception ex)
+            else
             {
-                consoleManager.centerText($"Error grabbing HWID. Error: {ex.Message}");
-                consoleManager.keepOpen();
-                Clipboard.SetText(ex.Message);
+                Console.Clear();
+                consoleManager.centerText("Pick a correct option retard");
+                Console.ReadLine();
+                Environment.Exit(0);
             }
         }));
         
         static void Main(string[] args)
         {
             consoleManager.moveWindowToCenter();
-            Console.Title = "@buse whitelister"; 
+            consoleWriter.writeLogo();
+            Console.Title = "@buse whitelister";
+            Console.WriteLine("\n");
+            consoleManager.centerText("Pick a project");
+            consoleWriter.optionsWriter("Minecraft");
+            consoleWriter.optionsWriter("Counter Strike");
+            var cheat = Console.ReadLine();;
+            hwidType = cheat;
             consoleManager.centerText("Grabbing HWID");
             thread.SetApartmentState(ApartmentState.STA);
             thread.Start();
---
+++ handler/consoleWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abuse.handler
+{
+    internal class consoleWriter
+    {
+        public static int options = 0;
+
+        public static void writeLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            consoleManager.centerText("        __                                ");
+            consoleManager.centerText(@"       /\ \                               ");
+            consoleManager.centerText(@"   __  \ \ \____  __  __    ____     __   ");
+            consoleManager.centerText(@" /'__`\ \ \ '__`\/\ \/\ \  /',__\  /'__`\ ");
+            consoleManager.centerText(@"/\ \L\.\_\ \ \L\ \ \ \_\ \/\__, `\/\  __/ ");
+            consoleManager.centerText(@"\ \__/.\_\\ \_,__/\ \____/\/\____/\ \____\");
+            consoleManager.centerText(@" \/__/\/_/ \/___/  \/___/  \/___/  \/____/");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        public static void optionsWriter(string text)
+        {
+            options++;
+            Console.WriteLine($"{options}.) {text}");
+        }
+
+        public static void resetOptions()
+        {
+            options = 0;
+        }
+    }
+}
